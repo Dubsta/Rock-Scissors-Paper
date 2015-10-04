32a3e801b1239a76c7ffa2f3065c3b4d05301383
Michael Atkinson <mikeatk88@gmail.com>
2015-10-04 12:39:51 +0900

    Add working bounce to gameResult, move player tag
    - Add jquery.ui files
    - Add bounce up and down depending on win / lose
    - Change tie result to function
    - Add extra div to #gameResult for quick bugfix
    - Small fix to spacing, comments in .js
    - Move player tag to bottom of container for symmetry

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -43,57 +43,61 @@ $("#player .choice").click(function() {
 		$("#computer .scissors").addClass("selectHighlight");
 	};
 
-	// Win or Lose function
+	// Win or Lose or tie functions
 	var win = "You win!";
 	function winner() {
 		console.log(win);
 		$("#player .selectHighlight").addClass("winHighlight");
 		$("#computer .selectHighlight").addClass("loseHighlight");
-		$("#gameResult").text(win).css("background", "#6F6")
-		// $("#gameResult").effect("bounce", "slow");
+		$("#gameResult div").text(win);
+		$("#gameResult").css("background", "#6F6").effect("bounce", "slow");
 	}
 	var lose = "You lose!";
 	function loser() {
 		console.log(lose);
 		$("#player .selectHighlight").addClass("loseHighlight");
 		$("#computer .selectHighlight").addClass("winHighlight");
-		$("#gameResult").text(lose).css("background", "#F66");
+		$("#gameResult div").text(lose);
+		$("#gameResult").css("background", "#F66").effect("bounce", {direction:"down"}, "slow");
+	}
+	var tie = "Tie!";
+	function tied() {
+		console.log("The result is a tie!");
+		$("#gameResult div").text(tie);
+		$("#gameResult").css("background", "#FF6");
 	}
 
-		//tie
+	// Case of tie
 	if (userChoice === computerChoice) {
-		console.log("The result is a tie!");
-		$("#gameResult").text("TIE").css("background", "#FF6");
+		tied();
 	}
-		//Rock
+	// Case of rock
 	else if (userChoice === "rock") {
 		if (computerChoice === "scissors") {
 			winner();
 		} else {
-			loser();
-			
+			loser();		
 	}}
-		//paper
+	// Case of paper
 	else if (userChoice === "paper") {
 		if (computerChoice === "scissors") {
 			loser();
 		} else {
 			winner();
 	}}
-
-		//scissors
+	// Case of scissors
 	else if (userChoice === "scissors") {
 		if (computerChoice === "rock") {
 			loser();
 		} else {
 			winner();
-		}
-	}
+	}}
 
 	// Clean up and end event, ready for next game (after 1.5sec)
 	setTimeout(function() {
 		$(".choice").removeClass("selectHighlight winHighlight loseHighlight");
-		$("#gameResult").text("...Choose Above").css("background", "#66F");
+		$("#gameResult div").text("...Choose Above...");
+		$("#gameResult").css("background", "#66F");
 		playing = false;
 	}, 1500);		
 //end of .click function
